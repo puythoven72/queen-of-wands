eae1661e43f61b094d781940f7cf17cb6c30bcb9
Peter Uythoven <puythoven72@gmail.com>
2024-08-20 15:12:28 -0400

    added a check to see if there are current events

--- src/components/EventsComponent.js
+++ src/components/EventsComponent.js
@@ -1,7 +1,30 @@
 import { Row, Col, Container } from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
-import currentEvent from "../assets/images/currentEvent.png";
+//import currentEvent from "../assets/images/currentEvent.png";
+import { useEffect, useState } from "react";
 function EventsComponent() {
+
+    const [messageDisplay, setMessageDisplay] = useState(null);
+    const [eventImgSrc, setEventImgSrc] = useState("");
+
+    useEffect(() => {
+        setImgSource()
+
+    }, [])
+
+    function setImgSource() {
+        try {
+            // const src = require('../assets/images/currentEvent1.png');
+            setEventImgSrc(require('../assets/images/currentEvent.png'));
+
+        }
+        catch (err) {
+            setEventImgSrc(require('../assets/images/stayTuned.png'));
+            setMessageDisplay("MORE EVENTS COMING SOON");
+        }
+
+    }
+
 
 
     return (
@@ -15,14 +38,16 @@ function EventsComponent() {
                 </Col>
             </Row>
             <Row className="mt-2 " >
-                <Col className="d-flex align-items-center justify-content-center text-center ">
-                    <Image src={currentEvent} thumbnail 
-                    style={{ borderRadius: '15px' ,border: '1px solid black'}}
-                    alt="Current Event"
+                <Col className="d-flex align-items-center justify-content-center text-center" id="imageCol">
+                    <Image src={eventImgSrc} 
+                        style={{ borderRadius: '15px', border: '1px solid black' }}
+                        alt="Current Event"
                     />
                 </Col>
-
             </Row>
+            {messageDisplay != null ? (<Row><Col className="splashTitle d-flex align-items-center justify-content-center text-center">{messageDisplay}</Col></Row>):("")}
+
+
         </Container>
     )
 

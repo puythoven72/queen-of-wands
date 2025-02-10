72c30d476d4aa9a5f64fbd1c6db7261f1a6dbce7
Peter Uythoven <puythoven72@gmail.com>
2025-02-10 14:24:01 -0500

    Because of some design decisions made earlier, no matter what url put in, the site always when to the home page. Figured out a hackey way to get to all the main pages from a url

--- src/App.js
+++ src/App.js
@@ -9,33 +9,79 @@ import EventsComponent from './components/EventsComponent';
 import HomeComponent from './components/HomeComponent';
 import TermsOfService from './components/TermsOfService';
 import EventComponent from './components/EventComponent';
+import { useEffect } from "react";
 import Footer from './components/Footer';
+
 function App() {
+
+  let currentUrl = window.location.href;
   const [activeLink, setActiveLink] = React.useState("/home");
- 
+
+
+  useEffect(() => {
+    getAllItems();
+  }, []);
+
+
+  async function getAllItems() {
+    await getCurrentUrl().then((data) => {
+      setActiveLink(data)
+    }).catch(error => { console.log(error) })
+  }
+
+
+  function getCurrentUrl() {
+
+    //let isCurrentUrl = currentUrl.includes("events");
+    if (currentUrl.includes("events")) {
+      return new Promise((resolve) => {
+        resolve('/events')
+      });
+    }
+    if (currentUrl.includes("services")) {
+      return new Promise((resolve) => {
+        resolve('/services')
+      });
+    }
+    if (currentUrl.includes("about")) {
+      return new Promise((resolve) => {
+        resolve('/about')
+      });
+    } if (currentUrl.includes("contact")) {
+      console.log("FOUND CONTACT");
+      return new Promise((resolve) => {
+        resolve('/contact')
+      });
+    }
+
+
+    return new Promise((resolve) => {
+      resolve('/home')
+    });
+
+  }
+
   return (
     <div >
-      
-      
-        <Router>
-        
-        <HeaderComponent  activeLink={activeLink} setActiveLink={setActiveLink}/>
+      <Router>
+
+        <HeaderComponent activeLink={activeLink} setActiveLink={setActiveLink} />
         <div className='footerPadding'>
           <Routes>
-            <Route path="/" element={<HomeComponent activeLink={activeLink} setActiveLink={setActiveLink}/>} />
-            <Route path="/home" element={<HomeComponent activeLink={activeLink} setActiveLink={setActiveLink}/>} />
+            <Route path="/" element={<HomeComponent activeLink={activeLink} setActiveLink={setActiveLink} />} />
+            <Route path="/home" element={<HomeComponent activeLink={activeLink} setActiveLink={setActiveLink} />} />
             <Route path="/services" element={<ServicesComponent activeLink={activeLink} setActiveLink={setActiveLink} />} />
             <Route path="/about" element={<AboutComponent />} />
-            <Route path="/contact" element={<ContactComponent navigation = "topNav"/>} />
+            <Route path="/contact" element={<ContactComponent navigation="topNav" />} />
             <Route path="/rsvp" element={<ContactComponent />} />
             <Route path="/events" element={<EventsComponent />} />
-            <Route path="/event" element={<EventComponent />} />
-            <Route path="/TermsOfService" element={<TermsOfService/>} />
+            <Route path="/displayEvent" element={<EventComponent />} />
+            <Route path="/TermsOfService" element={<TermsOfService />} />
           </Routes>
         </div>
-        <Footer activeLink={activeLink} setActiveLink={setActiveLink}/>
-        </Router>
-      
+        <Footer activeLink={activeLink} setActiveLink={setActiveLink} />
+      </Router>
+
 
 
 
--- src/components/EventsComponent.js
+++ src/components/EventsComponent.js
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
-
 import CardGroup from 'react-bootstrap/CardGroup';
 import { Link } from 'react-router-dom';
 import { Button } from "react-bootstrap";
@@ -15,8 +14,8 @@ function EventsComponent() {
 
     const [messageDisplay, setMessageDisplay] = useState(null);
     const [eventImgSrc, setEventImgSrc] = useState("");
-    const [allEvents,setAllEvents] = useState([]);
-  
+    const [allEvents, setAllEvents] = useState([]);
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -27,7 +26,6 @@ function EventsComponent() {
 
     const getAllEvents = () => {
         axios.get("/assets/json/events.json").then((res) => {
-
             setAllEvents(res.data.events);
         })
     };
@@ -35,19 +33,14 @@ function EventsComponent() {
 
     function setImgSource() {
         const eventImage = '/assets/currentEvent.png';
-        // let events = Events.currentEvents;
-
         axios
             .get(eventImage)
             .then(() => {
                 setEventImgSrc(eventImage);
             })
             .catch(() => {
-
                 setEventImgSrc(require('../assets/images/stayTuned.png'));
-
             });
-
     }
 
 
@@ -58,7 +51,6 @@ function EventsComponent() {
 
     return (
         <Container className="p-3">
-
             <Row className="mt-2">
                 <Col className="">
                     <h1 className="splashTitle  d-flex align-items-center justify-content-center text-center">
@@ -71,11 +63,11 @@ function EventsComponent() {
                 <CardGroup>
                     {allEvents.map((ev, index) => (
 
-                        <Card className='m-2' style={{ backgroundColor: "#3a415c", color: "#c8b568", borderRadius: '15px' }}>
+                        <Card key={index} className='m-2' style={{ backgroundColor: "#3a415c", color: "#c8b568", borderRadius: '15px' }}>
 
                             <Card.Title className="queenFont" >{ev.location} - {ev.date}</Card.Title >
 
-                            <Link className="eventCard" to="/event" state={{ data: JSON.stringify(ev) }} >
+                            <Link className="eventCard" to="/displayEvent" state={{ data: JSON.stringify(ev) }} >
                                 <Card.Img className="eventCard" variant="top" src={process.env.PUBLIC_URL + 'assets/images/' + ev.image} alt="me" />
                             </Link>
                             {/* <Card.Footer className="queenFont" style={{ backgroundColor: "#3a415c", color: "#c8b568" }}>QUEEN OF WANDS TAROT ADVISING</Card.Footer> */}

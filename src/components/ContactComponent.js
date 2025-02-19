15006c2256a972171b11284b4572d2dbfd7c7b5f
Peter Uythoven <puythoven72@gmail.com>
2025-02-19 17:42:02 -0500

    added MIT licence for reactdatepicker

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -14,6 +14,12 @@ import "react-datepicker/dist/react-datepicker.css"
 import CalandarUtility from "./utility/CalandarUtility";
 
 
+
+
+
+
+
+
 const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
 const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
 const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY;
@@ -66,7 +72,7 @@ function ContactComponent(props) {
             let rsvp = JSON.parse(rsvpdata);
             setContactTitle("RSVP For " + rsvp.location + " ON " + rsvp.date);
             // setRsvpMessage("I will be attending your event at " + rsvp.location + " on " + rsvp.date + "Queen of Wands will respond shortly confirming appointment time.");
-            document.getElementById("message").value = "I will be attending your event at " + rsvp.location + " on " + rsvp.date ;
+            document.getElementById("message").value = "I will be attending your event at " + rsvp.location + " on " + rsvp.date;
 
             //This will get day/month/year seperately
             let eventYear = CalandarUtility.getYear(rsvp.date);
@@ -270,6 +276,30 @@ function ContactComponent(props) {
 
                                 </Form.Group>
                                 ) : (null)
+                        }
+                        {
+                        /* react-datepicker
+                        The MIT License (MIT)
+
+                        Copyright (c) 2014-2025 HackerOne Inc and individual contributors
+
+                        Permission is hereby granted, free of charge, to any person obtaining a copy
+                        of this software and associated documentation files (the "Software"), to deal
+                        in the Software without restriction, including without limitation the rights
+                        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                        copies of the Software, and to permit persons to whom the Software is
+                        furnished to do so, subject to the following conditions:
+
+                        The above copyright notice and this permission notice shall be included in all
+                        copies or substantial portions of the Software.
+
+                        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                        SOFTWARE. */
                         }
                         {
                             isRSVP ?
